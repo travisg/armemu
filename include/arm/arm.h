0800d74b1d2b3fcb43f528da62bd77324c5c6ada
Travis Geiselbrecht <geist@foobox.com>
2012-06-02 10:21:55 -0700

    fix BITS() macro to finally get rid of that shift warning

--- include/arm/arm.h
+++ include/arm/arm.h
@@ -246,11 +246,10 @@ enum {
 #define ROR(val, shift) \
 	(((val) >> (shift)) | ((val) << (32 - (shift))))
 
-
 /* bit manipulation macros */
 #define BIT(x, bit) ((x) & (1 << (bit)))
 #define BIT_SHIFT(x, bit) (((x) >> (bit)) & 1)
-#define BITS(x, high, low) ((x) & (((1<<((high)+1))-1) & ~((1<<(low))-1)))
+#define BITS(x, high, low) ((x) & (((1<<((high)-(low)+1))-1) << (low)))
 #define BITS_SHIFT(x, high, low) (((x) >> (low)) & ((1<<((high)-(low)+1))-1))
 #define BIT_SET(x, bit) (((x) & (1 << (bit))) ? 1 : 0)
 
